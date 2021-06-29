a6282f22d50b85640f8bccc498e16bb242c218fd
shivang <shivanggupta287@gmail.com>
2021-06-29 16:27:58 +0530

    javascript added - nav, aside, footer

---
+++ javascript/footer.js
@@ -0,0 +1,38 @@
+var data = `
+        <div class="footer-row">
+            <div class="footer-column">
+                <h3>SOLUTIONS</h3>
+                <ul>
+                    <li><a href="/ncert-solution/ncert-solution.html#class-12-solution">NCERT Class-12</a> Solution pdf</li>
+                    <li><a href="/ncert-solution/ncert-solution.html#class-11-solution">NCERT Class-11</a> Solution pdf</li>
+                    <li><a href="/ncert-solution/ncert-solution.html#class-10-solution">NCERT Class-10</a> Solution pdf</li>
+                    <li><a href="/ncert-solution/ncert-solution.html#class-9-solution">NCERT Class-9</a> Solution pdf</li>
+                    </ul>
+            </div>
+            <div class="footer-column">
+                <h3>SAMPLE PAPERS</h3>
+                <ul>
+                    <li><a href="/cbse-sample-paper/sample-paper.html#class-12-samplepaper">CBSE Class-12</a> Sample Paper Download pdf</li>
+                    <li><a href="/cbse-sample-paper/sample-paper.html#class-11-samplepaper">CBSE Class-11</a> Sample Paper Download pdf</li>
+                    <li><a href="/cbse-sample-paper/sample-paper.html#class-10-samplepaper">CBSE Class-10</a> Sample Paper Download pdf</li>
+                    <li><a href="/cbse-sample-paper/sample-paper.html#class-9-samplepaper">CBSE Class-9</a> Sample Paper Download pdf</li>           
+                </ul>
+            </div>
+            <div class="footer-column">
+                <h3>BOOKS</h3>
+                <ul>
+                    <li><a href="/ncert-books/c12.html">NCERT Book</a> Download pdf Class-12</li>
+                    <li><a href="/ncert-books/c11.html">NCERT Book</a> Download pdf Class-11</li>
+                    <li><a href="/ncert-books/c10.html">NCERT Book</a> Download pdf Class-10</li>
+                    <li><a href="/ncert-books/c9.html">NCERT Book</a> Download pdf Class-9</li>
+                    <li><a href="/ncert-books/c8.html">NCERT Book</a> Download pdf Class-8</li>
+                    <li><a href="/ncert-books/c7.html">NCERT Book</a> Download pdf Class-7</li>
+                    <li><a href="/ncert-books/c6.html">NCERT Book</a> Download pdf Class-6</li>
+                    <li><a href="/ncert-books/c5.html">NCERT Book</a> Download pdf Class-5</li>
+                    <li><a href="/ncert-books/c4.html">NCERT Book</a> Download pdf Class-4</li>
+                    <li><a href="/ncert-books/c3.html">NCERT Book</a> Download pdf Class-3</li>
+                </ul>
+            </div>
+        </div>
+`
+document.write(data)
---
+++ javascript/nav.js
@@ -0,0 +1,14 @@
+var data = `
+            <ul class="navigation-list">
+                <li class="nav-list-item">
+                    <a href="/ncert-solution/ncert-solution.html">NCERT SOLUTION</a>
+                </li>
+                <li class="nav-list-item">
+                    <a href="/ncert-books/ncert-books.html">NCERT BOOKS</a>
+                </li>
+                <li class="nav-list-item">
+                    <a href="/cbse-sample-paper/sample-paper.html">NCERT SAMPLE PAPERS</a>
+                </li>
+            </ul>       
+`
+document.write(data)
